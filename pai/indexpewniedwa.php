01d7343c613f3f552aa6f6f539082e410ffc30ee
eichlerj <eichler.jedrzej@gmail.com>
2019-09-03 14:03:05 +0200

    satuq

--- pai/indexpewniedwa.php
+++ pai/indexpewniedwa.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>snipety - to takie snajperskie papieroski</title>
+    <title>snipety - to takie snajperskie papieroskia z ctrl i lewym robie fajne</title>
   </head>
   <body>
     <?php  ?>
