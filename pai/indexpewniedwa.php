6f6194e603ab5b01094d52935d89b521ccebfc45
eichlerj <eichler.jedrzej@gmail.com>
2019-09-03 13:46:41 +0200

    hanczyk nie wie ze jest gupi a maik mowie losowe gown co chwile

--- pai/indexpewniedwa.php
+++ pai/indexpewniedwa.php
@@ -1,3 +1,10 @@
-<?php
-echo 'maik to niby gupol a niby nie ale ja w sumie sie nie znam wiec nie wiem';
- ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>snipety - to takie snajperskie papieroski</title>
+  </head>
+  <body>
+    <?php  ?>
+  </body>
+</html>
